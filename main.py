ce4fedb1ceaf120146ff8f7353a958b1031198a1
Om Tambat <omtambat284@gmail.com>
2025-10-25 00:00:53 +0530

    Pose estimation implemented

--- main.py
+++ main.py
@@ -2,38 +2,29 @@ import cv2
 from ultralytics import YOLO
 
 # Create a new YOLO model from scratch
-model = YOLO("yolov8n.pt") 
+model = YOLO("yolov8n-pose.pt")
 
-cam = cv2.VideoCapture(1)
-
-if not cam.isOpened():
-    print("Cannot open camera")
-    exit()
+# Capture Video
+cap = cv2.VideoCapture(1)
 
 while True:
-    ret, frame = cam.read()
-    
+    ret, frame = cap.read()
+
     if not ret:
-        print("Failed to capture")
-
-    result = model(frame)
-
-    #Draw bounding boxes on persion
-
-    for box in result[0].boxes:
-        class_id = int(box.cls.cpu().numpy())
-        label = result[0].names[class_id]
-
-        if label == 'person':
-            #Get box coordinates
-            x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.putText(frame, 'Om', (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
-
-    cv2.imshow("DroidCam Feed", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cam.release()
+    # Perform pose estimation on the frame
+    results = model(frame)
+
+    # Get keypoints of in each frame and plot them
+    for r in results:
+        if r.keypoints is not None:
+            anotated_frame = r.plot()
+
+    cv2.imshow('YOLOv8 Pose Estimation', anotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
 cv2.destroyAllWindows()
